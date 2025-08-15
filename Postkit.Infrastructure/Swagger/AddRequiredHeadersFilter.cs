3fd1dc183500070e13d8d08d678daa3c8fc5f12d
John Ronel Dela Cruz <johnroneldc@gmail.com>
2025-08-15 20:51:12 +0800

    code cleanup

---
+++ Postkit.Infrastructure/Swagger/AddRequiredHeadersFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Postkit.Infrastructure.Swagger
+{
+    public class AddRequiredHeadersFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+                operation.Parameters = new List<OpenApiParameter>();
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "X-Tenant-Id",
+                In = ParameterLocation.Header,
+                Required = true,
+                Schema = new OpenApiSchema
+                {
+                    Type = "string"
+                },
+                Description = "Tenant ID for multi-tenant requests"
+            });
+        }
+    }
+}
